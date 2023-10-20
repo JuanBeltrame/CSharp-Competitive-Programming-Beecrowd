cea2390f90189294163b795947027c66a628ec06
Juan Beltrame <juan_beltrame@hotmail.com>
2023-10-20 10:35:16 -0300

    New Exercise

---
+++ (Beecrowd) Beginner - 1059 - Even Numbers/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+Link to the problem: https://www.beecrowd.com.br/judge/en/problems/view/1059
+----------------------------------------------------------------------
+Write a program that prints all even numbers between 1 and 100, including them if it is the case.
+
+Input
+In this extremely simple problem there is no input.
+
+Output
+Print all even numbers between 1 and 100, including them, one by row.
+*/
+
+using System.ComponentModel;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        for (int i = 1; i <= 100; i++)
+        {
+            if (i % 2 == 0)
+            {
+                System.Console.WriteLine(i);
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
